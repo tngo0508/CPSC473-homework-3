e728e7012bf65fa6055ea4e2fa991f1ff5ddb0e0
Thomas Ngo <tngo0508@gmail.com>
2018-03-02 21:38:35 -0800

    Finish chapter 10

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
 (function(window) {
   "use strict";
+  var FORM_SELECTOR = "[data-coffee-order=form]";
   var App = window.App;
   var Truck = App.Truck;
   var Datastore = App.Datastore;
+  var FormHandler = App.FormHandler;
   var myTruck = new Truck("ncc-1701", new Datastore());
   window.myTruck = myTruck;
+  var formHandler = new FormHandler(FORM_SELECTOR);
+
+  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  console.log(formHandler);
 })(window);

f2227394c253888bdafe8493e40abc2c7832f4c3
Thomas Ngo <tngo0508@gmail.com>
2018-03-03 18:26:47 -0800

    completed step 4

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,19 @@
 (function(window) {
   "use strict";
-  var FORM_SELECTOR = "[data-coffee-order=form]";
+  var FORM_PAYMENT = "[data-payment-form=form]";
+  // var FORM_SELECTOR = "[data-coffee-order=form]";
   var App = window.App;
   var Truck = App.Truck;
   var Datastore = App.Datastore;
   var FormHandler = App.FormHandler;
   var myTruck = new Truck("ncc-1701", new Datastore());
   window.myTruck = myTruck;
-  var formHandler = new FormHandler(FORM_SELECTOR);
+  // var formHandler = new FormHandler(FORM_SELECTOR);
 
-  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-  console.log(formHandler);
+  // formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+
+  var formhandler1 = new FormHandler(FORM_PAYMENT);
+  formhandler1.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  // console.log(formHandler);
+  console.log(formhandler1);
 })(window);

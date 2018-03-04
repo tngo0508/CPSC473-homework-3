e2732a98d5d652abcdc28b1e1f0efa1dcca451c8
Thomas Ngo <tngo0508@gmail.com>
2018-03-03 22:28:07 -0800

    Submit the form adds an item to the checklist

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,25 @@
 (function(window) {
   "use strict";
-  var FORM_PAYMENT = "[data-payment-form=form]";
-  // var FORM_SELECTOR = "[data-coffee-order=form]";
+  // var FORM_PAYMENT = "[data-payment-form=form]";
+  var FORM_SELECTOR = "[data-coffee-order=form]";
+  var CHECKLIST_SELECTOR = "[data-coffee-order=checklist]";
   var App = window.App;
   var Truck = App.Truck;
   var Datastore = App.Datastore;
   var FormHandler = App.FormHandler;
+  var CheckList = App.CheckList;
   var myTruck = new Truck("ncc-1701", new Datastore());
   window.myTruck = myTruck;
-  // var formHandler = new FormHandler(FORM_SELECTOR);
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
+  var formHandler = new FormHandler(FORM_SELECTOR);
 
-  // formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  formHandler.addSubmitHandler(function(data) {
+    myTruck.createOrder.call(myTruck, data);
+    checkList.addRow.call(checkList, data);
+  });
 
-  var formhandler1 = new FormHandler(FORM_PAYMENT);
-  formhandler1.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-  // console.log(formHandler);
-  console.log(formhandler1);
+  // var formhandler1 = new FormHandler(FORM_PAYMENT);
+  // formhandler1.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  console.log(formHandler);
+  // console.log(formhandler1);
 })(window);
